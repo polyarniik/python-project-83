6b9dea0d3b71479c4823358fda858391373cb6b5
Safiullin, Ruslan <rusl.safiullin@x5.ru>
2024-01-05 16:47:22 +0300

    change variable name

--- page_analyzer/config.py
+++ page_analyzer/config.py
@@ -5,4 +5,4 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DATABASE_DSN = os.getenv("DATABASE_DSN")
+DATABASE_DSN = os.getenv("DATABASE_URL")
